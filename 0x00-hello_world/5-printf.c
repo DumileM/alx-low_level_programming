95b143ac2b386193de2845c93a45ef44a1435b43
DumileM <mnenodumile@gmail.com>
2023-02-16 13:17:00 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is the main function
  *
- * Return: always 0
+ * Return: 0 when successful
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return(0);
+	return (0);
 }
