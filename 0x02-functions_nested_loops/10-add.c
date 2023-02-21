cccca89266c1699c9d7353065df918463ab26743
DumileM <mnenodumile@gmail.com>
2023-02-21 13:21:37 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: parameter_1
+ *
+ * @b: parameter_2
+ *
+ * Return: returns c
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
