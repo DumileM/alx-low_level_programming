6e6ffee1c857e4385bb940746e816e8b3691405f
DumileM <mnenodumile@gmail.com>
2023-02-22 12:09:54 -0800

    Task 2: 0>972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -26,4 +27,6 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
+
+	printf("\n");
 }
