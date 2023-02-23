1e475304d2f426fd219b517e90a01cae640751fc
DumileM <mnenodumile@gmail.com>
2023-02-23 12:50:40 -0800

    Task 3: The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ *
+ * @a: 1st parameter
+ *
+ * @b: 2nd parameter
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	printf(a * b)
+}
