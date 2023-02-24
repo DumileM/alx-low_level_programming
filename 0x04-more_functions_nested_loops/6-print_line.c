e35a694d41b20a59a807395d49823627738673ea
DumileM <mnenodumile@gmail.com>
2023-02-24 12:43:53 -0800

    Task 6: The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -5,20 +5,23 @@
  *
  * @n: parameter
  *
- * @a: parameter
- *
  * Return: always 0
  */
 
 void print_line(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
-
-		int a = 1;
+	{
+		int a;
 
 		for (a = 1; a <= n; a++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
+	}
 }
