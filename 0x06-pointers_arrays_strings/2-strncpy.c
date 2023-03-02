285afeb1043f4ee4851f1bfbff229a58e62c5912
DumileM <mnenodumile@gmail.com>
2023-03-02 09:33:27 -0800

    Task 2: strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ *
+ * @dest: string 1
+ *
+ * @src: string 2
+ *
+ * @n: variable 1
+ *
+ * Return: varies
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
