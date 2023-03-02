dc306f99db5c79543db971c8f8f617ea69e14f24
DumileM <mnenodumile@gmail.com>
2023-03-02 14:42:58 -0800

    Task 1: strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,12 +17,14 @@ char *_strncat(char *dest, char *src, int n)
 	int destlen = 0;
 	int srclen = 0;
 	int i;
+	int j;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlen++;
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
-	for (i = 0; i < n; i++)
-		dest[destlen + i] = src[i];
+	for (i = destlen, j = 0; j < n && dest[j] != '\0' && src[j] != '\0'; i++, j++)
+		dest[i] = src[j];
+	dest[i] = '\0';
 	return (dest);
 }
