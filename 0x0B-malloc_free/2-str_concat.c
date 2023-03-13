017d72be2b429459b4750004cb853c7bfe73b83b
DumileM <mnenodumile@gmail.com>
2023-03-13 12:37:25 -0700

    Task 2: He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *str_concat - function that concatenates two strings.
+ *
+ * @s1: string 1.
+ *
+ * @s2: string 2.
+ *
+ * Return: Failure (NULL).
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1, len2, i, j;
+	char *str;
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < len2; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[i] = '\0';
+
+	return (str);
+}
+
