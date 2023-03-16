6e6b7413954274c23ccfdd74f7621e1ab0d36fbb
DumileM <mnenodumile@gmail.com>
2023-03-16 10:30:19 -0700

    Task 1: string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -40,6 +40,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (n < len2 && i < (len1 + n))
 		str[i++] = s2[j++];
 
+	while (n >= len2 && i < (len1 + len2))
+		str[i++] = s2[j++];
+
 	str[i] = '\0';
 
 	return (str);
